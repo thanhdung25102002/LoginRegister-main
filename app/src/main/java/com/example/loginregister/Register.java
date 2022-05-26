3f7a76f8d9fa8698191661519bf0a7031cf0d4fb
Rango2 <97595370+phanvanduc02@users.noreply.github.com>
2022-05-26 19:02:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/loginregister/Register.java
@@ -0,0 +1,89 @@
+package com.example.loginregister;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Register extends AppCompatActivity {
+    private EditText etName, etEmail, etPassword;
+    private TextView tvStatus;
+    private Button btnRegister;
+    private String URL = "http://192.168.43.80/login/register.php";
+    private String name, email, password;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+        etName = findViewById(R.id.etName);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+
+        tvStatus = findViewById(R.id.tvStatus);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        name = email = password = "";
+
+    }
+
+    public void save(View view) {
+        name = etName.getText().toString().trim();
+        email = etEmail.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+
+        if(password == " "){
+            Toast.makeText(this, "Password Mismatch", Toast.LENGTH_SHORT).show();
+        }
+        else if(!name.equals("") && !email.equals("") && !password.equals("")){
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    if (response.equals("success")) {
+                        tvStatus.setText("Successfully registered.");
+                        btnRegister.setClickable(false);
+                    } else if (response.equals("failure")) {
+                        tvStatus.setText("Dang Ky Thanh cong!");                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+            }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> data = new HashMap<>();
+                    data.put("name", name);
+                    data.put("email", email);
+                    data.put("password", password);
+                    return data;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+        }
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
